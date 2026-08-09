f74724c6e0ca1bebe9834d88fed4b7eb6953edb2
nae-rms <wallaceramos2511@gmail.com>
2026-08-10 04:10:37 +0800

    ci fix

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="nae_ml_template",
     version="0.1.0",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
